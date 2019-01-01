c1301c1247d760aad4c723d37977d74366b21a3b
vvasuki <vishvas.vasuki@gmail.com>
2019-01-01 14:04:22 -0800

    Content update.

--- ashtadhyayi_data/reader/vritti_repo/__init__.py
+++ ashtadhyayi_data/reader/vritti_repo/__init__.py
@@ -12,12 +12,14 @@ logging.basicConfig(
     format="%(levelname)s:%(asctime)s:%(module)s:%(lineno)d %(message)s"
 )
 
+pandas.set_option('display.max_columns', 5)
+pandas.set_option('display.width', 1000)
 ASHTADHYAYI_REPO_ROOT = "/home/vvasuki/sanskrit/ashtadhyayi"
 
 def get_file_paths(vritti_id):
     return sorted(glob.glob(os.path.join(ASHTADHYAYI_REPO_ROOT, vritti_id, "*/*.md")))
 
-def get_vritti_sequence_map(vritti_id):
+def get_vritti_metadata_df(vritti_id):
     vritti_df = pandas.DataFrame(columns= ["index", "sutra", "vritti_index"])
     for file_path in get_file_paths(vritti_id=vritti_id):
         with open(file_path) as f:
@@ -27,8 +29,16 @@ def get_vritti_sequence_map(vritti_id):
                 vritti_df.loc[vritti_data["index"], "vritti_index"] = vritti_data["vritti_index"]
             # logging.debug(vritti_data)
     vritti_df.set_index("index")
-    logging.debug(vritti_df.loc["1.1.1"])
+    return vritti_df
+    # logging.debug(vritti_df.loc["1.1.1"])
+
+def get_vrittis_with_mismatching_sutra(vritti_id):
+    vritti_metadata_df = get_vritti_metadata_df(vritti_id=vritti_id)
+    from ashtadhyayi_data import sutra_df
+    filtered_df = vritti_metadata_df[vritti_metadata_df["sutra"].replace(to_replace="[ ऽ]", regex=True, value="") != sutra_df.loc[vritti_metadata_df["index"], 'sutra'].replace(to_replace="[ ऽ]", regex=True, value="")]
+    logging.debug(filtered_df)
+    pass
 
 
 if __name__ == '__main__':
-    get_vritti_sequence_map(vritti_id='balamanorama')
+    get_vrittis_with_mismatching_sutra(vritti_id='balamanorama')
