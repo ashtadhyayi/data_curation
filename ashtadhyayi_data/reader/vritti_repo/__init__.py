14280fccf4803a2df9bbccbea9e4041028038d1e
vvasuki <vishvas.vasuki@gmail.com>
2019-01-01 12:59:57 -0800

    Content update.

--- ashtadhyayi_data/reader/vritti_repo/__init__.py
+++ ashtadhyayi_data/reader/vritti_repo/__init__.py
@@ -1,3 +1,7 @@
+import glob
+import os
+import pandas
+
 import frontmatter
 import logging
 
@@ -8,10 +12,23 @@ logging.basicConfig(
     format="%(levelname)s:%(asctime)s:%(module)s:%(lineno)d %(message)s"
 )
 
+ASHTADHYAYI_REPO_ROOT = "/home/vvasuki/sanskrit/ashtadhyayi"
 
-def get_vRtti_sequence_map(vritti_id):
-    pass
+def get_file_paths(vritti_id):
+    return sorted(glob.glob(os.path.join(ASHTADHYAYI_REPO_ROOT, vritti_id, "*/*.md")))
+
+def get_vritti_sequence_map(vritti_id):
+    vritti_df = pandas.DataFrame(columns= ["index", "sutra", "vritti_index"])
+    for file_path in get_file_paths(vritti_id=vritti_id):
+        with open(file_path) as f:
+            vritti_data = frontmatter.load(f)
+            vritti_df.loc[vritti_data["index"]] = [vritti_data["index"], vritti_data["sutra"], None]
+            if "vritti_index" in vritti_data:
+                vritti_df.loc[vritti_data["index"], "vritti_index"] = vritti_data["vritti_index"]
+            # logging.debug(vritti_data)
+    vritti_df.set_index("index")
+    logging.debug(vritti_df.loc["1.1.1"])
 
 
 if __name__ == '__main__':
-    get_vRtti_sequence_map(vritti_id='balamanorama')
+    get_vritti_sequence_map(vritti_id='balamanorama')
